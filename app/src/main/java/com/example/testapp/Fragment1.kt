b7a15ead545bde6e7219e2599b1555c1702c10fd
ukulya <ukulya150992@gmail.com>
2021-11-15 17:28:22 +0600

    fourth commit

--- app/src/main/java/com/example/testapp/Fragment1.kt
+++ app/src/main/java/com/example/testapp/Fragment1.kt
@@ -1,7 +1,19 @@
 package com.example.testapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class Fragment1: Fragment() {
+class Fragment1: Fragment(R.layout.activity_main) {
+   /* override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        inflater.inflate(R.layout.activity_main,container)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }*/
 
 }
--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?) :Boolean {
+       menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this,item.title,Toast.LENGTH_LONG).show()
+        when(item.itemId){
+            val fragment1 =
+             R.id.menu_1 =-> {supportFragmentManager.beginTransaction()}
+        } else -?
+        return super.onOptionsItemSelected(item)
     }
 }
