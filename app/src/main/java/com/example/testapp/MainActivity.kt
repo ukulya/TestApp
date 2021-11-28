31b00df73c76b5aff5b16f90fb4b6dcf7df6f400
ukulya <ukulya150992@gmail.com>
2021-11-28 21:37:12 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,111 +1,64 @@
 package com.example.testapp
 
+
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.ViewCompat.setBackground
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import net.objecthunter.exp4j.ExpressionBuilder
+import com.example.testapp.MainActivity3.Companion.ACTION_1
 
 class MainActivity : AppCompatActivity() {
-    // TextView used to display the input and output
-    lateinit var txtInput: TextView
-
-    // Represent whether the lastly pressed key is numeric or not
-    var lastNumeric: Boolean = false
-
-    // Represent that current state is in error or not
-    var stateError: Boolean = false
-
-    // If true, do not allow to add another DOT
-    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtInput = findViewById(R.id.txtInput)
+
+        val btn1 = findViewById<AppCompatButton>(R.id.btn_1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn_2)
+        val btn3 = findViewById<AppCompatButton>(R.id.btn_3)
+        val btn4 = findViewById<AppCompatButton>(R.id.btn_4)
+        val btn5 = findViewById<AppCompatButton>(R.id.btn_5)
+
+        btn1.setOnClickListener { goTo1() }
+        btn2.setOnClickListener { goToWeb() }
+        btn3.setOnClickListener { goToGallery() }
+        btn4.setOnClickListener { goToActivityWithAction1() }
+        btn5.setOnClickListener { goToActivityWithAction2() }
     }
-    /**
-    * Append the Button.text to the TextView
-    */
-    fun onDigit(view: View) {
-        if (stateError) {
-            // If current state is Error, replace the error message
-            txtInput.text = (view as Button).text
-            stateError = false
+
+    private fun goTo1() {
+        val intent = Intent("myAction")
+        startActivity(intent)
+    }
+
+    private fun goToWeb() {
+        val address = Uri.parse("http://google.com")
+        val intent = Intent(Intent.ACTION_VIEW, address)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
         } else {
-            // If not, already there is a valid expression so append to it
-            txtInput.append((view as Button).text)
-        }
-        // Set the flag
-        lastNumeric = true
-    }
-
-    /**
-     * Append . to the TextView
-     */
-    fun onDecimalPoint(view: View) {
-        if (lastNumeric && !stateError && !lastDot) {
-            txtInput.append(".")
-            lastNumeric = false
-            lastDot = true
+            Toast.makeText(this, "asdasdas", Toast.LENGTH_SHORT).show()
         }
     }
 
-    /**
-     * Append +,-,*,/ operators to the TextView
-     */
-    fun onOperator(view: View) {
-        if (lastNumeric && !stateError) {
-            txtInput.append((view as Button).text)
-            lastNumeric = false
-            lastDot = false    // Reset the DOT flag
-        }
+    private fun goToGallery() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.type = "image/*"
+        startActivity(intent)
     }
 
-
-    /**
-     * Clear the TextView
-     */
-    fun onClear(view: View) {
-        this.txtInput.text = ""
-        lastNumeric = false
-        stateError = false
-        lastDot = false
+    private fun goToActivityWithAction1() {
+        val intent = Intent(ACTION_1)
+        intent.putExtra("key", "CCCc")
+        startActivity(intent)
     }
 
-    /**
-     * Calculate the output using Exp4j
-     */
-    fun onEqual(view: View) {
-        // If the current state is error, nothing to do.
-        // If the last input is a number only, solution can be found.
-        if (lastNumeric && !stateError) {
-            // Read the expression
-            val txt = txtInput.text.toString()
-            // Create an Expression (A class from exp4j library)
-            val expression = ExpressionBuilder(txt).build()
-            try {
-                // Calculate the result and display
-                val result = expression.evaluate()
-                txtInput.text = result.toString()
-                lastDot = true // Result contains a dot
-            } catch (ex: ArithmeticException) {
-                // Display an error message
-                txtInput.text = "Error"
-                stateError = true
-                lastNumeric = false
-            }
-        }
+    private fun goToActivityWithAction2() {
+        val intent = Intent("action2")
+        intent.putExtra("key", "XXXx")
+        startActivity(intent)
     }
 }
