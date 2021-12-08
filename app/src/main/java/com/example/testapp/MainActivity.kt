9ecf3dfd7af7dbf899d742188e59466c86a34550
ukulya <ukulya150992@gmail.com>
2021-12-08 17:18:41 +0600

    second commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,23 +1,11 @@
 package com.example.testapp
 
-
-import android.annotation.SuppressLint
-import android.content.ClipDescription
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    val email = findViewById<AppCompatEditText>(R.id.email)
-    val subject = findViewById<AppCompatEditText>(R.id.subject)
-    val text = findViewById<AppCompatEditText>(R.id.text)
     val btn = findViewById<AppCompatButton>(R.id.btn)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,28 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn.setOnClickListener {
-            val recipient = email.text.toString().trim()
-            val subj = subject.text.toString().trim()
-            val message = text.text.toString().trim()
-            sendEmail(recipient,subj,message)
+            startActivity(Intent(this,MainActivity2::class.java))
         }
-
-    }
-
-    private fun sendEmail(recipient: String,subj: String,message: String) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.data = Uri.parse("mailto:")
-        intent.type = "plain/text"
-        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
-        intent.putExtra(Intent.EXTRA_SUBJECT, subj)
-        intent.putExtra(Intent.EXTRA_TEXT, message)
-
-        try {
-            startActivity(Intent.createChooser(intent,"Choose email client"))
-        } catch (e: Exception){
-            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
-        }
-
 
     }
 
